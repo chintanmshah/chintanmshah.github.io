b35d5301e8b5a9db7b8570e40099f33c33cb9656
Chintan Shah <cshahhhh@gmail.com>
2020-12-27 17:06:57 +0530

    Changing it

--- wedding/scripts.js
+++
@@ -1,6 +0,0 @@
-var today = new Date();
-var weddingDate = new Date("02/16/2021");
-
-var TimeDiff = Math.ceil(Math.max(weddingDate - today, 0) / (1000 * 3600 * 24));
-
-document.getElementById("days-left").innerHTML = TimeDiff + ((TimeDiff == 1) ? " day" : " days") + " left";
